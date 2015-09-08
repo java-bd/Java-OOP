b1b6e2f2a7fda3db07cf13d2f981b651a658e25a
netcse <smamran09@gmail.com>
2015-09-08 11:13:53 +0600

    dummy message

---
+++ overloading/MainMethodOverloading.java
@@ -0,0 +1,12 @@
+// http://www.javatpoint.com/method-overloading-in-java
+
+class MainMethodOverloading{  
+  public static void main(int a){  
+	System.out.println(a);  
+  }  
+    
+  public static void main(String args[]){  
+	  System.out.println("main() method invoked");  
+	  main(10);  
+  }  
+} 
